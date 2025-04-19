2a1a608a9f30e541808fa952055935f2223a1f69
cFabij <105910968+cFabij@users.noreply.github.com>
2025-04-19 14:02:15 +0200

    limit fps

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -12,6 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
